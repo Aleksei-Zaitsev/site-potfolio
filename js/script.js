78cc4bad1d6e494801fd613c5304df79218264d6
Aleksei Zaitsev <Aleksei.Zaitsev.V@yandex.ru>
2020-12-18 02:41:28 +0300

    second-commit

--- js/script.js
+++ js/script.js
@@ -137,4 +137,117 @@ document.addEventListener('DOMContentLoaded', () => {
         thanksModal.classList.add('modal-swipe');
     };
 
+
+    // Potfolio tabs 
+
+    const portfolioBtn1 = document.querySelectorAll('.portfolio-item-text')[0],
+        portfolioBtn2 = document.querySelectorAll('.portfolio-item-text')[1],
+        portfolioBtn3 = document.querySelectorAll('.portfolio-item-text')[2],
+        portfolioBtn4 = document.querySelectorAll('.portfolio-item-text')[3],
+        portfolioCard1 = document.querySelectorAll('.portfolio_card_area')[0],
+        portfolioCard2 = document.querySelectorAll('.portfolio_card_area')[1],
+        portfolioCard3 = document.querySelectorAll('.portfolio_card_area')[2],
+        portfolioCard4 = document.querySelectorAll('.portfolio_card_area')[3],
+        portfolioCardBtnClose1 = document.querySelectorAll('.porfolio_card_close')[0],
+        portfolioCardBtnClose2 = document.querySelectorAll('.porfolio_card_close')[1],
+        portfolioCardBtnClose3 = document.querySelectorAll('.porfolio_card_close')[2],
+        portfolioCardBtnClose4 = document.querySelectorAll('.porfolio_card_close')[3];
+    
+
+
+    // 1
+
+    portfolioBtn1.addEventListener('click', openPorfolioCard1);
+
+    portfolioCard1.addEventListener('click', (e) => {
+        console.log(e.target);
+        if (e.taget === portfolioCard1 || e.target.classList.contains('card_active')) {
+            closePortfolioCard1();
+        }
+    });
+
+    portfolioCardBtnClose1.addEventListener('click', closePortfolioCard1);
+
+
+    function openPorfolioCard1() {
+        portfolioCard1.classList.add('card_active');
+        document.body.style.overflow = 'hidden';
+    }
+
+    function closePortfolioCard1() {
+        portfolioCard1.classList.remove('card_active');
+        document.body.style.overflow = '';
+    }
+
+    //2
+
+    portfolioBtn2.addEventListener('click', openPorfolioCard2);
+
+    portfolioCard2.addEventListener('click', (e) => {
+        console.log(e.target);
+        if (e.taget === portfolioCard2 || e.target.classList.contains('card_active')) {
+            closePortfolioCard2();
+        }
+    });
+
+    portfolioCardBtnClose2.addEventListener('click', closePortfolioCard2);
+
+
+    function openPorfolioCard2() {
+        portfolioCard2.classList.add('card_active');
+        document.body.style.overflow = 'hidden';
+    }
+
+    function closePortfolioCard2() {
+        portfolioCard2.classList.remove('card_active');
+        document.body.style.overflow = '';
+    }
+
+    // 3
+
+    portfolioBtn3.addEventListener('click', openPorfolioCard3);
+
+    portfolioCard3.addEventListener('click', (e) => {
+        console.log(e.target);
+        if (e.taget === portfolioCard3 || e.target.classList.contains('card_active')) {
+            closePortfolioCard3();
+        }
+    });
+
+    portfolioCardBtnClose3.addEventListener('click', closePortfolioCard3);
+
+
+    function openPorfolioCard3() {
+        portfolioCard3.classList.add('card_active');
+        document.body.style.overflow = 'hidden';
+    }
+
+    function closePortfolioCard3() {
+        portfolioCard3.classList.remove('card_active');
+        document.body.style.overflow = '';
+    }
+
+    // 4
+
+    portfolioBtn4.addEventListener('click', openPorfolioCard4);
+
+    portfolioCard4.addEventListener('click', (e) => {
+        console.log(e.target);
+        if (e.taget === portfolioCard4 || e.target.classList.contains('card_active')) {
+            closePortfolioCard4();
+        }
+    });
+
+    portfolioCardBtnClose4.addEventListener('click', closePortfolioCard4);
+
+
+    function openPorfolioCard4() {
+        portfolioCard4.classList.add('card_active');
+        document.body.style.overflow = 'hidden';
+    }
+
+    function closePortfolioCard4() {
+        portfolioCard4.classList.remove('card_active');
+        document.body.style.overflow = '';
+    }
 });
